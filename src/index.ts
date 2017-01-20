615a79101b964a2b34d4291b40f55fce9249fd06
Lopierdis <liadz0rz@gmail.com>
2017-01-20 17:47:24 +0200

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,41 @@
 type Point = createjs.Point;
 let Point = createjs.Point;
 
-class Main extends createjs.Stage {
-    graphRenderer: GraphRenderer;
+class Intersection{
+    public roads: Road[];
+    location: Point;
+    constructor(x: number, y: number){
+        this.roads = [];
+        this.location = new Point(x,y);
+    }
+}
 
-    constructor (g: Graph) {
+class Road {
+    constructor (public intersection1: Intersection, public intersection2: Intersection) {
+        intersection1.roads.push(this);
+        intersection2.roads.push(this);
+    }
+}
+
+class City{
+    public intersections: Intersection[];
+}
+
+class Map {
+    public roads: Road[] = [];
+    public cities: City[] = [];
+}
+
+class Main extends createjs.Stage {
+    graphRenderer: MapRenderer;
+
+    constructor (map: Map) {
         super ('main-canvas');
 
-        this.graphRenderer = new GraphRenderer ();
+        this.graphRenderer = new MapRenderer ();
         this.addChild (this.graphRenderer);
 
-        this.graphRenderer.render (g);
+        this.graphRenderer.render (map);
     }
 
     update () {
@@ -19,61 +44,51 @@ class Main extends createjs.Stage {
     }
 }
 
-class Edge {
-    constructor (public node1: Point, public node2: Point) {}
-}
-
-class Graph {
-    nodes: Point[];
-    edges: Edge[];
-}
-
-let ex_graph = new Graph ();
-ex_graph.nodes = [new Point (935/2,100/2),
-                  new Point (1370/2,105/2),
-                  new Point (1188/2,392/2),
-                  new Point (934/2,400/2),
-                  new Point (1197/2,645/2),
-                  new Point (1587/2,954/2),
-                  new Point (663/2,542/2),
-                  new Point (664/2,802/2),
-                  new Point (892/2,688/2),
-                  new Point (405/2,962/2),
-                  new Point (908/2,933/2),
-                  new Point (1166/2,970/2)];
-ex_graph.edges = [new Edge (ex_graph.nodes[1], ex_graph.nodes[0]),
-                  new Edge (ex_graph.nodes[1], ex_graph.nodes[2]),
-                  new Edge (ex_graph.nodes[0], ex_graph.nodes[3]),
-                  new Edge (ex_graph.nodes[2], ex_graph.nodes[3]),
-                  new Edge (ex_graph.nodes[5], ex_graph.nodes[4]),
-                  new Edge (ex_graph.nodes[4], ex_graph.nodes[2]),
-                  new Edge (ex_graph.nodes[3], ex_graph.nodes[6]),
-                  new Edge (ex_graph.nodes[6], ex_graph.nodes[7]),
-                  new Edge (ex_graph.nodes[8], ex_graph.nodes[4]),
-                  new Edge (ex_graph.nodes[8], ex_graph.nodes[7]),
-                  new Edge (ex_graph.nodes[7], ex_graph.nodes[9]),
-                  new Edge (ex_graph.nodes[7], ex_graph.nodes[10]),
-                  new Edge (ex_graph.nodes[8], ex_graph.nodes[10]),
-                  new Edge (ex_graph.nodes[10],ex_graph.nodes[11])];
-
-class GraphRenderer extends createjs.Shape {
-    render (graph: Graph) {
+class MapRenderer extends createjs.Shape {
+    render (map: Map) {
         let g = this.graphics;
         g.clear ();
-        for (let node of graph.nodes) {
+        for (let city of map.cities) {
             g.beginFill ('black');
-            console.log (node);
-            g.drawCircle (node.x, node.y, 5);
-            g.endFill ()
+            g.drawCircle (city.intersections[0].location.x, city.intersections[0].location.y, 5);
+            g.endFill ();
         }
-        for (let edge of graph.edges) {
+        for (let road of map.roads) {
             g.beginStroke ('black');
-            g.moveTo (edge.node1.x, edge.node1.y);
-            g.lineTo (edge.node2.x, edge.node2.y);
+            g.moveTo (road.intersection1.location.x, road.intersection1.location.y);
+            g.lineTo (road.intersection2.location.x, road.intersection2.location.y);
             g.endStroke ();
         }
     }
 }
+
+let ex_graph = new Map ();
+let intersections = [new Intersection (935/2,100/2),
+                  new Intersection (1370/2,105/2),
+                  new Intersection (1188/2,392/2),
+                  new Intersection (934/2,400/2),
+                  new Intersection (1197/2,645/2),
+                  new Intersection (1587/2,954/2),
+                  new Intersection (663/2,542/2),
+                  new Intersection (664/2,802/2),
+                  new Intersection (892/2,688/2),
+                  new Intersection (405/2,962/2),
+                  new Intersection (908/2,933/2),
+                  new Intersection (1166/2,970/2)];
+ex_graph.roads = [new Road (intersections[1], intersections[0]),
+                  new Road (intersections[1], intersections[2]),
+                  new Road (intersections[0], intersections[3]),
+                  new Road (intersections[2], intersections[3]),
+                  new Road (intersections[5], intersections[4]),
+                  new Road (intersections[4], intersections[2]),
+                  new Road (intersections[3], intersections[6]),
+                  new Road (intersections[6], intersections[7]),
+                  new Road (intersections[8], intersections[4]),
+                  new Road (intersections[8], intersections[7]),
+                  new Road (intersections[7], intersections[9]),
+                  new Road (intersections[7], intersections[10]),
+                  new Road (intersections[8], intersections[10]),
+                  new Road (intersections[10],intersections[11])];
 
 let main = new Main (ex_graph);
 main.update ();
