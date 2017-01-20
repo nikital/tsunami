14b54cbda40dc8c56f08414b4d86d94edc1d7603
Lopierdis <liadz0rz@gmail.com>
2017-01-20 13:24:46 +0200

    Configured gulp, added src folder with tsconfig

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,21 @@
-v
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var path = require('path');
+var sourcemaps = require('gulp-sourcemaps');
+var shell = require('gulp-shell')
+
+gulp.task('build:client', function () {
+    var tsProject = ts.createProject(path.resolve('./src/tsconfig.json'));
+    var tsResult = gulp.src([
+        'src/**/*.ts'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+    return tsResult.js
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.resolve('./out')))
+});
+
+gulp.task('build', ['build:client']);
+
+gulp.task('default', ['build']);
